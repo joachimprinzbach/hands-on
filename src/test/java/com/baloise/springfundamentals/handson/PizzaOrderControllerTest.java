ef68c0a75752f2252e9d641cc25fafaae5983050
Joachim Prinzbach <joachim.prinzbach@gmail.com>
2020-01-21 21:34:11 +0100

    Remove unused imports

--- src/test/java/com/baloise/springfundamentals/handson/PizzaOrderControllerTest.java
+++ src/test/java/com/baloise/springfundamentals/handson/PizzaOrderControllerTest.java
@@ -3,19 +3,14 @@ package com.baloise.springfundamentals.handson;
 import com.baloise.springfundamentals.handson.persistence.PizzaOrder;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Collections;
 
-import static org.hamcrest.Matchers.containsString;
 import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
